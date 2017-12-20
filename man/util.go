0196664d479576d7ea0dcdf27f569956e70a5b14
Ray Johnson <ray.johnson@gmail.com>
2017-12-20 11:58:55 -0800

    Added logic for Examples section

--- man/util.go
+++ man/util.go
@@ -12,8 +12,8 @@ func (s byName) Len() int           { return len(s) }
 func (s byName) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
 func (s byName) Less(i, j int) bool { return s[i].Name() < s[j].Name() }
 
-
 var sanatizeReplacer *strings.Replacer
+
 func simpleToTroff(str string) string {
 	// Guessing this is already troff - so let it pass through
 	if str[0] == '.' {
@@ -29,10 +29,10 @@ func simpleToTroff(str string) string {
 }
 
 var backslashReplacer *strings.Replacer
+
 func backslashify(str string) string {
 	if backslashReplacer == nil {
 		backslashReplacer = strings.NewReplacer("-", "\\-", "_", "\\_", "&", "\\&", "\\", "\\\\", "~", "\\~")
 	}
 	return backslashReplacer.Replace(str)
 }
-
