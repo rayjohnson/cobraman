e1305a8664d21e9313ea8ac2f4e40916a066d23f
Ray Johnson <ray.johnson@gmail.com>
2017-12-30 11:29:52 -0800

    Added util_test.go

---
+++ man/util_test.go
@@ -0,0 +1,54 @@
+package man
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestBackslashify(t *testing.T) {
+	cases := [][]string{
+		{`foo-bar`, `foo\-bar`},
+		{`foo&bar`, `foo\&bar`},
+		{`foo_bar`, `foo\_bar`},
+		{`foo\bar`, `foo\\bar`},
+		{`foo~bar`, `foo\~bar`},
+		{`-_&\~`, `\-\_\&\\\~`},
+	}
+
+	for i := 0; i < len(cases); i++ {
+		str := backslashify(cases[i][0])
+		expected := cases[i][1]
+		assert.Equal(t, expected, str)
+	}
+}
+
+func TestDashify(t *testing.T) {
+	cases := [][]string{
+		{`foo bar`, `foo-bar`},
+		{`foo bar cat`, `foo-bar-cat`},
+		{` foo bar `, `-foo-bar-`},
+	}
+
+	for i := 0; i < len(cases); i++ {
+		str := dashify(cases[i][0])
+		expected := cases[i][1]
+		assert.Equal(t, expected, str)
+	}
+}
+
+func TestSimpleToTroff(t *testing.T) {
+	cases := [][]string{
+		{"Some test\none a line", "Some test\none a line"},
+		{"Some test\n\nwith empty line", "Some test\n.PP\nwith empty line"},
+		{".ignore me\n\none a line", ".ignore me\n\none a line"},
+		// TODO: these two tests do not work
+		// {"Some test\n\n\nwith empty line", "Some test\n.PP\nwith empty line"},
+		// {"Some test\n\n\n\nwith empty line", "Some test\n.PP\n.PP\nwith empty line"},
+	}
+
+	for i := 0; i < len(cases); i++ {
+		str := simpleToTroff(cases[i][0])
+		expected := cases[i][1]
+		assert.Equal(t, expected, str)
+	}
+}
