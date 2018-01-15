1b9d56ed1ece66883853628bff8168444b336edf
Ray Johnson <ray.johnson@gmail.com>
2018-01-14 22:37:02 -0800

    Added tool generator functionality

---
+++ man/tool.go
@@ -0,0 +1,97 @@
+// Copyright © 2018 Ray Johnson <ray.johnson@gmail.com>.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package man
+
+import (
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+)
+
+// DocGenTool is an opaque type created by CreateDocGenCmdLineTool
+type DocGenTool struct {
+	installDirectory string
+	docCmd           *cobra.Command
+	appCmd           *cobra.Command
+}
+
+// CreateDocGenCmdLineTool creates a command line parser that can be used
+// in a utility tool to generate documentation for a companion application.
+func CreateDocGenCmdLineTool(appCmd *cobra.Command) *DocGenTool {
+	dg := &DocGenTool{
+		appCmd: appCmd,
+	}
+
+	dg.docCmd = &cobra.Command{
+		Use:   "doc",
+		Args:  cobra.NoArgs,
+		Short: "Generate documentation, etc.",
+	}
+	dg.docCmd.PersistentFlags().StringVar(&dg.installDirectory, "directory", ".", "Directory to install generated files")
+
+	return dg
+}
+
+// AddBashCompletionGenerator will create a subcommand for the utility tool
+// that will generate a Bash Completion file for the companion app.  It will
+// support a --directory flag and use the fileName passed into this function.
+func (dg *DocGenTool) AddBashCompletionGenerator(fileName string) *DocGenTool {
+
+	completeCmd := &cobra.Command{
+		Use:   "generate-auto-complete",
+		Args:  cobra.NoArgs,
+		Short: "Generate bash auto complete script",
+		RunE: func(myCmd *cobra.Command, args []string) error {
+			path := filepath.Join(dg.installDirectory, fileName)
+			return dg.appCmd.GenBashCompletionFile(path)
+		},
+	}
+
+	dg.docCmd.AddCommand(completeCmd)
+
+	return dg
+}
+
+// AddDocGenerator will create a subcommand for the utility tool that will
+// generate documentation with the passed in CobraManoptions and templateName.
+// It supports a --directory flag for where to place the generated files.  The
+// subcommand will be named generate-<templateName> where templateName is the
+// same as the template used to generate the documentation.
+func (dg *DocGenTool) AddDocGenerator(opts *CobraManOptions, templateName string) *DocGenTool {
+	// Make sure template exists or we will later get runtime panic
+	_, ok := templateMap[templateName]
+	if !ok {
+		panic("the given template has not been registered: " + templateName)
+	}
+
+	genCmd := &cobra.Command{
+		Use:   "generate-" + templateName,
+		Args:  cobra.NoArgs,
+		Short: "Generate docs with the " + templateName + " template",
+		RunE: func(myCmd *cobra.Command, args []string) error {
+			opts.Directory = dg.installDirectory
+			opts.TemplateName = templateName
+			return GenerateManPages(dg.appCmd, opts)
+		},
+	}
+
+	dg.docCmd.AddCommand(genCmd)
+
+	return dg
+}
+
+// Execute will parse args and execute the command line
+func (dg *DocGenTool) Execute() error {
+	return dg.docCmd.Execute()
+}
---
+++ man/tool_test.go
@@ -0,0 +1,85 @@
+// Copyright © 2018 Ray Johnson <ray.johnson@gmail.com>.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package man
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateDocGenCmdLineTool(t *testing.T) {
+	appCmd := &cobra.Command{}
+	dg := CreateDocGenCmdLineTool(appCmd)
+	assert.NotNil(t, dg, "CreateDocGenCmdLineTool should not return nil")
+}
+
+func TestAddDocGenerator(t *testing.T) {
+	appCmd := &cobra.Command{}
+	dg := CreateDocGenCmdLineTool(appCmd)
+
+	opts := &CobraManOptions{}
+
+	// Template does not exist
+	assert.Panics(t, func() { dg.AddDocGenerator(opts, "foo") })
+	dg.AddDocGenerator(opts, "mdoc") // This one does exist
+
+	// bad sub-command
+	args := []string{"generate-bar"}
+	dg.docCmd.SetArgs(args)
+	buf := new(bytes.Buffer)
+	dg.docCmd.SetOutput(buf)
+
+	// No error is thrown instead usage string is shown
+	assert.NoError(t, dg.Execute())
+	assert.Regexp(t, "Available Commands.+\n.+generate-mdoc", buf)
+
+	buf.Reset()
+	args = []string{"generate-mdoc"}
+	dg.docCmd.SetArgs(args)
+	assert.Error(t, dg.Execute()) // Will generate an error because our command is dumb
+}
+
+func TestAddBashCompletionGenerator(t *testing.T) {
+	appCmd := &cobra.Command{}
+	dg := CreateDocGenCmdLineTool(appCmd)
+	dg.AddBashCompletionGenerator("foo.txt")
+}
+
+func TestExecute(t *testing.T) {
+	appCmd := &cobra.Command{Use: "foo"}
+	cmd2 := &cobra.Command{Use: "child1", Run: func(cmd *cobra.Command, args []string) {}}
+	cmd3 := &cobra.Command{Use: "child2", Run: func(cmd *cobra.Command, args []string) {}}
+	appCmd.AddCommand(cmd2, cmd3)
+
+	dg := CreateDocGenCmdLineTool(appCmd)
+	opts := &CobraManOptions{}
+	dg.AddDocGenerator(opts, "mdoc")
+	dg.AddDocGenerator(opts, "markdown")
+	dg.AddBashCompletionGenerator("foo.txt")
+
+	args := []string{"generate-markdown"}
+	dg.docCmd.SetArgs(args)
+	assert.NoError(t, dg.Execute())
+	checkForFile(t, "foo.md")
+	checkForFile(t, "foo_child1.md")
+	checkForFile(t, "foo_child2.md")
+
+	args = []string{"generate-auto-complete"}
+	dg.docCmd.SetArgs(args)
+	assert.NoError(t, dg.Execute())
+	checkForFile(t, "foo.txt")
+}
